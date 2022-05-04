dd52a3f519918af0941d50630c3076a8a65a2d34
Ian Sorensen <iasoren@pm.me>
2022-05-03 23:03:05 -0400

    File Cleanup
    Worked a bit more on LEDs, started initial firmware settings. Ordered some nice!nanos and soon to have prototype boards in to work with

--- firmware/lily58/keymaps/orvisevans/rules.mk
+++
@@ -1,8 +0,0 @@
-# If you want to change the display of OLED, you need to change here
-SRC +=  ./glowEnc/lib/rgb_state_reader.c \
-        ./glowEnc/lib/layer_state_reader.c \
-        ./glowEnc/lib/logo_reader.c \
-        ./glowEnc/lib/keylogger.c \
-        # ./lib/mode_icon_reader.c \
-        # ./lib/host_led_state_reader.c \
-        # ./lib/timelogger.c \
